be94f3e9a10bffd35680b5e0ab669c09df37f325
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-08-30 20:01:53 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function moveDodgerLeft(){
+  const dodger= document.getElementById('dodger');
+  const leftNumber = dodger.style.left.replace('px','');
+  const left = parseInt(leftNumber,10);
+  if(left>0){dodger.style.left=`${left -1}px`;}
+  // dodger.style.backgroundColor='#FF69B4';
+  // dodger.style.bottom='0px';
+  // dodger.style.left='0px';
+ 
+}
+function moveDodgerRight(){
+  const dodger= document.getElementById('dodger');
+  const leftNumber = dodger.style.left.replace('px','');
+  const left = parseInt(leftNumber,10);
+  if(left>0){dodger.style.left=`${left +1}px`;}
+}
+document.addEventListener('keydown',(event)=>{
+  if(event.key=='ArrowLeft'){
+    moveDodgerLeft();
+  }else if(event.key=='ArrowRight'){
+    moveDodgerRight();
+  }
+});
